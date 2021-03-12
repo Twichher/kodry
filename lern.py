ac8145d9e2293f500af68e33bc70a57de136613e
Twichher <73023145+Twichher@users.noreply.github.com>
2021-03-12 20:08:46 +0400

    Add files via upload

---
+++ lern.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+a = [['*'] * m for i in range(n)]
+
+for i in range(n):
+	for t in range(m):
+		a[i][t] = i + t + 2
+
+a[n-1], a[0] = a[0], a[n-1]
+
+for i in a:
+	print(*i)
+
+# print(*a[n-1])
+
+# for i in a[1:n-1]:
+# 	print(*i)
+
+# print(*a[0])
